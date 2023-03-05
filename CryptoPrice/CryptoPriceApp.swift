6a7a1efb276e334e082c213d587ab59fa2c1674c
Mohammed Mubashir <mohammedmubashir204@gmail.com>
2023-03-05 14:48:29 +0530

    Initial Commit

---
+++ CryptoPrice/CryptoPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPriceApp.swift
+//  CryptoPrice
+//
+//  Created by Mohammed Mubashir on 05/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
